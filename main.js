82bb2f57186e55077d3fbc2552e38cca061b2b63
jooa <dlwndk4677@gmail.com>
2022-07-12 19:19:01 -0700

    dawn shim

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
 var http = require('http');
-var fs = require('fs');  //file system 
-var url = require('url');
-
-
-var http = require('http');
 var fs = require('fs');
+const { url } = require('inspector');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/login.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+
+    if(_url == '/'){
+      title = "Welcome"
     }
-    if(request.url == '/Give'){
-      url = '/Give.html';
+
+    if(_url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
     }
-    
     response.writeHead(200);
     response.end(fs.readFileSync(__dirname + url));
+
+    fs.readFile(`data/${title}`, 'utf8', (err, description) => {        
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+      <title>WEB1 - HTML</title>
+      <meta charset="utf-8">
+      </head>
+      <body>
+      <h1><a href="/">WEB - ${title}</a></h1>
+      <ol>
+          <li><a href="?id=HTML">HTML</a></li>
+          <li><a href="?id=CSS">CSS</a></li>
+          <li><a href="?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p>
+          ${description}
+      </p>
+      </body>
+      </html>
+      `        
+      response.end(template);
+  })
  
 });
-app.listen(3000);
 
+app.listen(3000);
