32db159544576b05bfe4614d3bdcfb797ffe68a9
jooa <dlwndk4677@gmail.com>
2022-07-14 15:15:20 -0700

    get post

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
+const exp = require('constants');
 const app = express();
-const server = http.createServer(app);
-server.listen(3000);
-
-// app.get('/', function(request, response){
-//   response.send("hihi")
-// })
+var bodyParser = require('body-parser');
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
 
 app.get('/', function(request, response){
   response.sendFile(__dirname + "/public/login.html")
@@ -22,3 +20,30 @@ app.get('/give', function(request, response){
 })
 
 app.use(express.static('public'))
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
+app.post('/login', (req, res) => {
+  // res.send(req.body.id)
+  // res.send(req.body.pw)
+  console.log(req.body);
+  res.send(`<span>${req.body.id}</span><p></p><span>${req.body.pw}</span>`)
+
+});
+
+
+
+// app.get('/login', (req, res) => {
+//   res.send(`<span>${req.query.id}</span><p></p><span>${req.query.pw}</span>`)
+//   console.log(req.query)
+// });
+
+const server = http.createServer(app);
+server.listen(3000);
+
+
+
