dea3f5be2bf72a19652f9f328a784c9ef668a194
jooa <dlwndk4677@gmail.com>
2022-07-14 21:11:13 -0700

    continue

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const path = require('path');
-const express = require('express');
 const exp = require('constants');
+const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
-// app.set('view engine', 'ejs');
-// app.set('views', './views');
+const fs = require('fs');
+const { ppid } = require('process');
+
 
 app.get('/', function(request, response){
   response.sendFile(__dirname + "/public/login.html")
@@ -13,37 +14,56 @@ app.get('/', function(request, response){
 
 app.get('/request', function(request, response){
   response.sendFile(__dirname + "/public/Request.html")
+  console.log('sent');
 })
 
 app.get('/give', function(request, response){
   response.sendFile(__dirname + "/public/Give.html")
 })
+//
 
 app.use(express.static('public'))
-// app.use(express.json());
-// app.use(express.urlencoded({extended: false}));
-
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+var users = {
+  user: {id: 'user'}
+}
 
-app.post('/login', (req, res) => {
-  // res.send(req.body.id)
-  // res.send(req.body.pw)
-  console.log(req.body);
-  res.send(`<span>${req.body.id}</span><p></p><span>${req.body.pw}</span>`)
-
-});
-
-
-// //using get method 
-// app.get('/login', (req, res) => {
-//   res.send(`<span>${req.query.id}</span><p></p><span>${req.query.pw}</span>`)
-//   console.log(req.query)
+// app.get('/login', function(req, res){
+//   res.render('login');
 // });
+
+
+// app.post('/login', (req, res) => {
+//   var ID = req.body.id
+//   var PW = req.body.pw
+//   res.send(`<span>${ID}</span><p></p><span>${PW}</span>`)
+//   console.log(req.body);
+//   var info = req.body;
+
+// });
+
+app.post('/login', function(req, res, next){
+  console.log(req.body);
+  var ID = req.body.id
+  var PW = req.body.pw
+  res.send(`<span>${ID}</span><p></p><span>${PW}</span>`)
+  
+  res.redirect('/request');
+})
+
+app.use(express.urlencoded({extended: false}))
+
+
 
 const server = http.createServer(app);
 server.listen(3000);
 
-
+// const app2 = express();
+// app2.set('view engine', 'ejs');
+// app.post('/Request.ejs', (req, res)=> {
+//   const ID = req.body.id;
+//   res.render('Request', {id});
+// })
 
