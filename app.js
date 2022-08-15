c7d90871ded44ec56a71433cc4adc53919a1542a
jooa <dlwndk4677@gmail.com>
2022-08-16 00:41:03 +0900

    commit

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
-const app = require('express')();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+// var router = express.Router()
+var router = require('./routes/index')
+// var path = require('path')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var session = require('express-session')
+var flash = require('connect-flash')
 
-
-app.get('/', (req, res) => {
-    res.render('index');
-  });
-
-
-app.post('/login', (req, res) => {
-    res.render('login') // login.ejs
-});
-
-app.post('/register', (req,res) => {
-    res.render('register') //register.ejs
-    
-})
 app.listen(3000, () => {
-    console.log('3000 port');
+    console.log('3000 port is running');
 });
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs');
+
+module.exports = router;
+
+
+// app.set('views', 'views');
+
+//세션관련 (use 메소드에 등록)
+app.use(session({
+    secret: 'keyboard cat', //세션 암호화하기위한 키값 (다른문자열 넣어도 상관없음)
+    resave: false,
+    saveUninitialized: true
+}))
+
+//passport 관련 - 초기화 해줘야됨 
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash()) //express connect-flash (메시지를 쉽게 전달해줌)
+app.use(router)
+
+
+
+
+
+
+
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var path = require('path');
+var mysql = require('mysql');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy
+
+
+
+//DATA BASE setting 
+var connection = mysql.createConnection({
+  host: "localhost", 
+  user: "root",
+  password: "dlwndk4677",
+  database : 'users'
+})
+
+connection.connect();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('get join url');
+  res.render('register.ejs');
+  // res.render('index', { title: 'Express' });
 });
+
+
+passport.use('local-join', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done){
+  console.log('local-join callback called');
+}))
 
 module.exports = router;
